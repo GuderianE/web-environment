0f8398967b9e1a20396a57dde4db6f9ddd2474cd
Elias Guderian <46761652+GuderianE@users.noreply.github.com>
2021-10-30 15:18:04 +0200

    changes

---
+++ localplay.tf
@@ -0,0 +1,8 @@
+locals {
+    m1 = {
+        butter = "marygold"
+        meat = "pork"
+    }
+
+    l1 = ["blue", "green", "yellow"]
+}
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "ec2-instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~>2.0"
-  subnet_ids = "${lookup(local.private_subnets.id)}"
+  subnet_id = "${element(module.vpc.private_subnets, 0)}"
   ami = "${var.ami}"
   name = "webserver"
   instance_type = "${var.ec2_instance_type["${var.environment}"]}"
